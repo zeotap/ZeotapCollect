db478fa56dabf3e2cf433397b1f496fbe1aa05c1
Karthik <karthik.datta@zeotap.com>
2023-04-03 11:21:26 +0530

    8899 package file configuration

--- Package.swift
+++ Package.swift
@@ -4,25 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "ios-packager",
+    name: "ZeotapCollect",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ios-packager",
-            targets: ["ios-packager"]),
+            name: "ZeotapCollect",
+            targets: ["ZeotapCollect"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "ios-packager",
-            dependencies: []),
-        .testTarget(
-            name: "ios-packagerTests",
-            dependencies: ["ios-packager"]),
-    ]
+    
+      targets: [
+          // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+          // Targets can depend on other targets in this package, and on products in packages this package depends on.
+              .binaryTarget(
+                  name: "ZeotapCollect",
+                  url: "https://content.zeotap.com/ios-sdk/ios-collect-sdk.zip",
+                  checksum: "a52eb741548d3bbdd1cc209f5170693d629e2415a09fb5f4903b334be6f1cc9e")
+          ]
 )
