60e38921f3bf4e4716e5411561df5132096bc55a
Krishnaveni M <52656293+krishnaveni-zeotap@users.noreply.github.com>
2024-05-30 14:29:43 +0530

    ZCM-386 Update version to 1.3.2 (#15)
    * ZP-17339 Update version to 1.3.2
    
    * ZP-17339 Update version to 1.3.2
    
    * ZCM-386 Update package swift file

---
+++ ZeotapCollect.xcframework/ZeotapCollect.xcframework/ios-arm64/ZeotapCollect.framework/Headers/ZeotapCollect.h
@@ -0,0 +1,19 @@
+//
+//  ZeotapSDK.h
+//  ZeotapSDK
+//
+//  Created by Krishnaveni M on 24/08/21.
+//
+
+#import <Foundation/Foundation.h>
+#import <CommonCrypto/CommonCrypto.h>
+
+//! Project version number for ZeotapSDK.
+FOUNDATION_EXPORT double ZeotapCollectVersionNumber;
+
+//! Project version string for ZeotapSDK.
+FOUNDATION_EXPORT const unsigned char ZeotapCollectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZeotapSDK/PublicHeader.h>
+
+
