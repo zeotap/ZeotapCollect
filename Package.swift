7129d53ce94dcd9a6fa94eae4e8c01b69caeb15a
krishnaveni-zeotap <krishnaveni.m@zeotap.com>
2025-07-01 22:41:39 +0530

    Update url

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
           // Targets can depend on other targets in this package, and on products in packages this package depends on.
               .binaryTarget(
                   name: "ZeotapCollect",
-                  url: "https://content.zeotap.com/ios-sdk/ios-collect-sdk.zip",
+                  url: "https://content.zeotap.com/ios-sdk/v1.3.7/ios-collect-sdk.zip",
                   checksum: "e654f21d60d6d10908654047fe3e56a035dc5440f4c82934cdb8107ce697c20d")
           ]
 )
