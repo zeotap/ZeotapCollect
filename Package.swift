63f439411918859e9499fea871c6ada7730da17b
krishnaveni-zeotap <krishnaveni.m@zeotap.com>
2025-07-16 21:47:42 +0530

    Updated ZeotapCollect version to 1.3.8

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
           // Targets can depend on other targets in this package, and on products in packages this package depends on.
               .binaryTarget(
                   name: "ZeotapCollect",
-                  url: "https://content.zeotap.com/ios-sdk/ios-collect-sdk.zip",
-                  checksum: "6ce395120092f3c7b817839a479cb963b21c02ca4e77f7e5eaeb3c6f3e295aba")
+                  url: "https://content.zeotap.com/ios-sdk/v1.3.8/ios-collect-sdk.zip",
+                  checksum: "db3a6741a35828332f51798625ce1447a7c43d39307258fe6c271ff9668d4224")
           ]
 )
